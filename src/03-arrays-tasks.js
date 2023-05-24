263ee893d07acc77933e8cc42cdc1cc0b5b670a4
SvetlanaLashuk <svetlana.lashuk311@gmail.com>
2023-05-24 07:54:02 +0300

    feat: add getTail and fix get3TopItems solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -174,8 +174,8 @@ function getHead(arr, n) {
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  return arr.slice(arr.length - n);
 }
 
 
@@ -286,7 +286,7 @@ function propagateItemsByPositionIndex(/* arr */) {
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
 function get3TopItems(arr) {
-  return arr.sort((a, b) => b - a).splice(0, 3);
+  return arr.sort((a, b) => b - a).slice(0, 3);
 }
 
 
