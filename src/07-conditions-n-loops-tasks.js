b6355c93a792c2be57d9f9a7086926224fa93bba
SvetlanaLashuk <svetlana.lashuk311@gmail.com>
2023-05-24 15:25:28 +0300

    feat: add reverseString, reverseInteger, getDigitalRoot solutions

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -217,8 +217,8 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 
@@ -234,8 +234,8 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  return Number(num.toString().split('').reverse().join(''));
 }
 
 
@@ -277,8 +277,10 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const arrayOfDigits = num.toString().split('');
+  const sumOfDigits = arrayOfDigits.reduce((acc, value) => acc + Number(value), 0);
+  return sumOfDigits > 9 ? getDigitalRoot(sumOfDigits) : sumOfDigits;
 }
 
 
