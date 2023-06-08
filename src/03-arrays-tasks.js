9f82961562928d5632acadafb9bc5dec85265292
SvetlanaLashuk <svetlana.lashuk311@gmail.com>
2023-06-08 12:56:39 +0300

    feat: add getFalsyValuesCount, findAllOccurrences solutions

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -352,8 +352,8 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.filter((elem) => !elem).length;
 }
 
 /**
@@ -370,8 +370,8 @@ function getFalsyValuesCount(/* arr */) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.filter((elem) => elem === item).length;
 }
 
 /**
