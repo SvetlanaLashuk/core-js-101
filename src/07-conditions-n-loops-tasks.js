93405dea133e17dfbd50724b1f6d14b430627847
SvetlanaLashuk <svetlana.lashuk311@gmail.com>
2023-07-11 08:55:36 +0300

    feat: add isCreditCardNumber solution

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -263,8 +263,22 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const cardNum = ccn.toString();
+  let second = false;
+  let sum = 0;
+  for (let i = cardNum.length - 1; i >= 0; i -= 1) {
+    let digit = parseInt(cardNum[i], 10);
+    if (second) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+    sum += digit;
+    second = !second;
+  }
+  return sum % 10 === 0;
 }
 
 /**
